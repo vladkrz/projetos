dcb84950ac50e5a34ec80b60c53a38648643dc3c
jeffer01 <jeffersonafsouza@gmail.com>
2022-05-22 02:56:17 -0300

    att

--- loginPage/main.py
+++ loginPage/main.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template,request,session,url_for,redirect
 #from functools import wraps
 from flask_mysqldb import MySQL
 import os
-#Cria uma instância/aplicativo da classe Flask 
+#Cria uma instância/aplicativo da classe Flask com o nome atual do arquivo
 app = Flask(__name__)
 
 #Configura uma conexão com o MySQL
@@ -14,61 +14,97 @@ app.config['MYSQL_USER']='adm'
 app.config['MYSQL_PASSWORD']='1234'
 #Banco de dados a ser conectado
 app.config['MYSQL_DB']='Login'
-#Faz com que os dados sejam apresentados como dicionário chave-valor ao inves de tupla
+#Faz com que os dados sejam apresentados como dicionário chave-valor ao invés de tupla
 #Torna mais fácil de trabalhar
 app.config['MYSQL_CURSORCLASS']='DictCursor'
 #Inicializa/instancia o MySQL
 mysql = MySQL(app)
 
 
-
-#Criando uma rota para raiz
+#	Mapeando uma URL para vincular com função a ser renderizado
+#	Criando uma rota para raiz
 @app.route('/') 	
-#Decorator junto com a função para definir  o comportamento lógico da página 
-@app.route('/login', methods=['GET', 'POST']) #Para a página de login, o methods lida com solicitação POST ou GET
+#	Decorator junto com a função para definir  o comportamento lógico da página 
+#	Para a página de login, o methods lida com solicitação POST ou GET
+#	Esta rota está vinculada a função login()
+@app.route('/login', methods=['GET', 'POST']) 
 def login():
-	#Variável de erro para retorno de status de Login Correto ou de Login Incorreto
+	#	Variável de erro para retorno de mensagem de Login Correto ou de Login Incorreto
 	msg = ''
-	#Verifica o método real(GET ou POST) emitido pelo sistema
+	#	Verifica o método http da página(GET ou POST) emitido pelo sistema
 	if request.method == 'POST':
-			#Recebe os valores passados no form de usuário e senha
+			#	Recebe os valores passados no form de usuário e senha
 			username = request.form['username']
 			password = request.form['password']
-			#Habilita o Flask a realizar operações SQL
+			#	Habilita o Flask a realizar operações SQL
 			cursor = mysql.connection.cursor()
-			#Executa a consulta/comando no nosso banco e verifica os dados passados no form se é igual ao do banco Login
-			#Na interpolação de strings(username=%s) deve-se passar as variaveis entre parenteses(tupla)
+			'''
+			Executa a consulta/comando no nosso banco e verifica os dados passados 
+			no form se é igual ao do banco Login
+			Na interpolação de strings(username=%s) deve-se passar as variaveis entre 
+			parenteses(tupla)
+			'''
 			cursor.execute('SELECT * FROM Login WHERE username=%s AND password=%s',(username, password))
-			'''Armazena em account os dados da consulta executado pelo execute() 
+			'''
+			Armazena em account os dados da consulta executado pelo execute() 
 			que retornar do banco para a variável cursor.fetchone(que utiliza o 
-			método mysql.connection.cursor()) '''
-			account = cursor.fetchone() #fetchone Faz busca uma linha por vez 
+			método mysql.connection.cursor())
+			'''
+			account = cursor.fetchone() #	fetchone busca uma linha por vez na consulta
 
-			#Verifica se há dados retornados 
+			#	Verifica se há dados retornados 
 			if account:
-				'''#Armazena dados do usuário no objeto sessão associando um par chave-valor
-				onde a chave username recebe a variável username(allan)'''
-				#Logge_in é opcional. Caso queira trabalhar melhor com flags de sessão como logado e não logado
+				#	Logged_in é opcional. Caso queira trabalhar melhor com 
+				#	flags de sessão como logado e não logado
 				session['logged_in'] = True
+				'''
+				Armazena dados do usuário no objeto sessão associando um par chave-valor
+				onde a chave username recebe a variável username(allan)
+				'''
 				session['username'] = account['username']
 				msg = 'Logado com sucesso!'
+				'''
+				A variável na função render_template o torna acessível a colocarmos
+				no template html, no caso o index.html
+				É importante que os templates html estejam na pasta templates e no
+				mesmo diretório do arquivo principal da aplicação python 
+				já que o render_template busca os templates na pasta templates
+				
+				Templates ou modelos são arquivos que contem dados estáticos e espaços
+				reservados para dados dinamicos. Esses dados dinamicos são manipulados pelo
+				Jinja2. O documento final é produzido e renderizado pelo jinja2.
+				'''
+				#	render_template renderiza o arquivo html usando a modelagem Jinja2
 				return render_template('index.html', msg=msg)
 			else:
 				msg='Login inválido. Seu usuário ou senha estão incorretos. Tente novamente!'
 				
 	return render_template('login.html',msg=msg)
 
-#Criando uma rota para logout com a função logout
+#	Criando uma rota para logout com a função logout
 @app.route('/logout')
-def logout(): #Criando uma lógica para a 
-	#Libera a variável de sessão passando o usuário da sessão. None para se caso não ache
+def logout(): #	Criando uma lógica para a 
+	#	Remove a chave session['username'] que armazena o usuário do sistema
+	# 	None para se caso não ache
 	session.pop('username',None)
-	#Redireciona após remover o usuário da sessão para a página de login
+	#	Redireciona após remover o usuário da sessão para a página de login
+	#	O redirecionamento é feito com redirect e url_for, onde ao inves de passarmos /login
+	#	passamos apenas 'login' para ser redirecionado para essa URL
 	return redirect(url_for('login'))
 	
-#Verifica se a aplicação está sendo executado no mesmo diretório/pacote	
+#	Verifica se a aplicação está sendo executado no mesmo arquivo(atual)	
 if __name__ == '__main__':
-	#Chave secreta para aplicação Flask (qualquer string pode ser colocada)
-    app.secret_key= os.urandom(12)
-	#Habilita a inicialização da aplicação por cli python3 <arquivo>
-    app.run(debug=True)
+	'''
+	Chave secreta para aplicação Flask
+	Importante para as sessões de usuário e evitar alterações nos cookies de usuário
+	A chave secreta realmente deve ser algo seguro e forte ao invés de um os.urandom
+	ou uma string simples
+	'''
+	app.secret_key= os.urandom(12)
+	#	Inicia o servidor web de desenvolvimento
+	#	Para uma implantação de produção, use um servidor da Web pronto para produção,
+	#	como gunicorn ou uWSGI .
+	app.run(debug=True)
+	
+
+    
